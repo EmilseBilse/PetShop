6da79cf34fbf3f99c3d0838e726a8f444a0d8dfc
Emil Hallum Hansen <emil875b@easv365.dk>
2021-08-25 09:37:14 +0200

    Initial commit

---
+++ NtombizodwaTech.PetShop.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NtombizodwaTech.PetShop.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
